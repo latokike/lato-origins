99eddfe8994f3df838ac63503e2eddd7e1e69e00
Latokike <81393451+Latokike@users.noreply.github.com>
2021-04-04 23:16:37 -0700

    Add damage sources

---
+++ src/main/java/latokike/latoorigins/common/registry/LODamageSources.java
@@ -0,0 +1,10 @@
+package latokike.latoorigins.common.registry;
+
+import io.github.apace100.origins.mixin.DamageSourceAccessor;
+import net.minecraft.entity.damage.DamageSource;
+
+public class LODamageSources {
+
+    public static final DamageSource BOIL_IN_WATER = ((DamageSourceAccessor)((DamageSourceAccessor)DamageSourceAccessor.createDamageSource("boil_in_water")).callSetBypassesArmor()).callSetUnblockable();
+    public static final DamageSource HORNED = ((DamageSourceAccessor)((DamageSourceAccessor)DamageSourceAccessor.createDamageSource("horned")).callSetBypassesArmor()).callSetUnblockable();
+}
